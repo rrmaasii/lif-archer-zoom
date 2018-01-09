85ca1f80334cde9a3ef6110b0afbd2ddd2a730ad
Reece Maas <reecemaas@gmail.com>
2018-01-09 14:54:21 -0600

    Initial commit

---
+++ archerzoom.cs
@@ -0,0 +1,14 @@
+﻿new ActionMap(zoomMap);
+zoomMap.bind(mouse0, "ctrl button1",toggleU);
+function toggleU(%val)
+{
+        if (%val)
+        {
+            setFov((20));
+        }
+        else
+        {
+            setFov((70));
+        }
+}
+zoomMap.push();
